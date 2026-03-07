69d41d178f22c69ba2ae080e1fc609899809c1db
bsk.dev <maiscod09@gmail.com>
2026-03-07 14:25:01 +0100

    Create start.bat

---
+++ start.bat
@@ -0,0 +1,13 @@
+@echo off
+title 2xOneFakeDbGenerator - Lancement Administrateur
+
+echo.
+echo Demande des droits administrateur...
+echo.
+
+:: Chemin complet vers PowerShell (corrige l'erreur "non reconnu")
+"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -Command "Start-Process python.exe -ArgumentList '-u \"%~dp0\main.py\"' -Verb RunAs -Wait"
+
+echo.
+echo Programme terminé. Tu peux fermer cette fenêtre.
+pause
